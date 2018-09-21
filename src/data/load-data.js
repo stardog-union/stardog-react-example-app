9ec507b6a4832c030d74c8d458e8057644ee3a53
Jason Rogers <jason@stardog.com>
2018-09-21 15:23:35 -0400

    Add data, constants, and helpers (end Step 3)

---
+++ src/data/load-data.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const fs = require('fs');
+const { query, db } = require('stardog');
+const { conn, dbName } = require("../helpers/constants");
+const { wrapWithResCheck } = require("../helpers/wrapWithResCheck");
+
+const data = fs.readFileSync(path.join(__dirname, "data.ttl"), "utf8");
+const insertQuery = `insert data { ${data} }`;
+
+const logSuccess = () => console.log(`Created ${dbName}.\n`);
+const logFailure = failureReason => console.error(failureReason);
+
+// The "main" method for this script.
+const loadData = () => {
+  console.log(`Creating ${dbName}...\n`);
+
+  return db
+    .create(conn, dbName)
+    .then(wrapWithResCheck(() => query.execute(conn, dbName, insertQuery)))
+    .then(logSuccess)
+    .catch(logFailure);
+};
+
+loadData();
+
